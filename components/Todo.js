c711d151afd85555af131ee7f5fc013f3eed6c5e
Martin Rolph <martinalexrolph@gmail.com>
2018-06-24 15:53:33 +0200

    Basic class-based todo component with bindings

--- components/Todo.js
+++ components/Todo.js
@@ -1,3 +1,24 @@
 import React from 'react';
 
-export default () => <div>Implement me!</div>;
+export default class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { todoText, complete } = this.props.todo;
+    const { todoIndex, listIndex, toggleComplete } = this.props;
+    
+    return (
+      <div>
+        <input
+          type="checkbox"
+          checked={complete}
+          onChange={() => toggleComplete(listIndex, todoIndex)}
+        />
+
+        {todoText}
+      </div>
+    );
+  }
+}
